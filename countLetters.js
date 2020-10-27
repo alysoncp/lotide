807e7b4a003700be371281b187b668c2254394c6
alysoncp <alyson.c.pickett@gmail.com>
2020-10-27 20:13:47 +0000

    initial commit of countLetter.js. assertEqual.js updated

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly =  function(string) {
+  let letterCount = {};
+  for (let letter of string) {
+    if (letter !== ' ') {
+      if (letterCount[letter]) {
+        letterCount[letter] += 1;
+      } else {
+        letterCount[letter] = 1;
+      }
+    }
+  }
+
+  return letterCount;
+};
+
+console.log(countOnly("Lighthouse Labs"));
